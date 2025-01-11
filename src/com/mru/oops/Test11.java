67f1e71a483957357a45619b162287a0c299cb46
dsageethika <2311cs010571@mallareddyuniversity.ac.in>
2025-01-11 11:41:57 +0530

    OOPS

---
+++ src/com/mru/oops/Test11.java
@@ -0,0 +1,8 @@
+package com.mru.oops;
+
+public class Test11 {
+String message = "Geethu";
+public void m1() {
+	System.out.println("Geethu1");
+}
+}
---
+++ src/com/mru/oops/Test12.java
@@ -0,0 +1,8 @@
+package com.mru.oops;
+
+public class Test12 {
+String msg="Geeths";
+public void m2() {
+	System.out.println("Geeths");
+}
+}
